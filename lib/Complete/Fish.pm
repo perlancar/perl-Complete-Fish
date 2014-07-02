d4baa8e4edc205d06344ced7d6f99e832d8fb530
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-02 15:32:19 +0700

    Initial commit

---
+++ lib/Complete/Fish.pm
@@ -0,0 +1,21 @@
+package Complete::Fish;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Completion module for Fish shell
+
+=head1 DESCRIPTION
+
+Not yet implemented. Land grab :)
+
+
+=head1 SEE ALSO
+
+As far as I know, at the time of this writing, there's currently no CPAN modules
+related to fish shell, let alone fish tab completion.
+
+=cut
